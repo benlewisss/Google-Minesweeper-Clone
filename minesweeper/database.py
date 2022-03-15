757d4a226907df8646e9d6c15c1ef7d8d74b1b80
Ben <41802624+benlewisss@users.noreply.github.com>
2022-03-15 10:15:42 +0000

    Add files via upload

--- minesweeper/database.py
+++ minesweeper/database.py
@@ -67,8 +67,7 @@ class Database():
 
 		cur = conn.cursor()
 
-		sql_insert_user = """ INSERT INTO users(username, join_date)
-				VALUES(?,?) """
+		sql_insert_user = """ INSERT INTO users(username, join_date) VALUES(?,?) """
 		user_tuple = (username, self.timestamp)
 		
 		cur.execute("SELECT * FROM users WHERE username=?", (username,))
@@ -110,7 +109,7 @@ class Database():
 		cur = conn.cursor()
 
 		sql_users_query = """ SELECT * FROM users """
-		sql_scores_query = """ SELECT * FROM scores ORDER BY user_id ASC"""
+		sql_scores_query = """ SELECT * FROM scores ORDER BY difficulty ASC"""
 		
 		cur.execute(sql_users_query)
 		records = cur.fetchall()
@@ -127,29 +126,29 @@ class Database():
 			print(record)  
 
 	def get_leaderboard(self):
-		# :return: Ordererd list containing sub-lists ordered by score. Index 0 is the highest score.
-		# :return_format: [[username, highscore, difficulty, timestamp], [username, highscore, difficulty, timestamp], etc...]
+		# :return: list[0,1,2] is difficulty 0,1,2 respectively. list[x][0,1,2] is the top 3 scores for that difficulty 
+		#          respectively, list[x][x][0,1,2] is user_id, score and date respectively
+		# :return_format: [[(username, highscore, difficulty, timestamp)], [(username, highscore, difficulty, timestamp)], etc...]}
 		conn = self.conn
 
 		cur = conn.cursor()
 
+		sql_query = """ SELECT user_id, MIN(score), date 
+						FROM scores 
+						WHERE difficulty=? 
+						GROUP BY user_id 
+						ORDER BY score ASC 
+						LIMIT 3"""
+
 		leaderboard = []
+		for difficulty in range(3):
+			cur.execute(sql_query,(difficulty,))
+			results = cur.fetchall()
+			leaderboard.append(results)
 
-		sql_users_query = """ SELECT * FROM users """
-		
-		cur.execute(sql_users_query)
-		users = cur.fetchall()
-		for user in users:
-			cur.execute("SELECT MIN(score), difficulty, date FROM scores WHERE user_id=?", (user[0],))
-			result = cur.fetchone()
-			entry = [user[1], result[0], result[1], result[2]]
-
-			leaderboard.append(entry)
-
-		leaderboard.sort(key=take_score)
 		return leaderboard
 
-	def get_highscore(self, user_id):
+	def get_highscore(self, difficulty, user_id):
 		# :param: ID of the user retrieving their highscore SQL@users.id
 		# :return: Tuple containing the highscore and the timestamp when it was achieved.
 		# :return_format: return = (score, difficulty, timestamp)
@@ -158,7 +157,7 @@ class Database():
 
 		cur = conn.cursor()
 
-		cur.execute("SELECT MIN(score), difficulty, date FROM scores WHERE user_id=?", (user_id,))
+		cur.execute("SELECT MIN(score), difficulty, date FROM scores WHERE user_id=? AND difficulty=?", (user_id,difficulty,))
 		result = cur.fetchone()
 		return result
 
@@ -195,30 +194,34 @@ class Database():
 		
 
 
-# db = Database("data/database.sqlite")
+db = Database("data/database.sqlite")
 
 # print(db.get_user(3))
 
 # db.purge_data()
 
-# userID1 = db.create_user("BenLewis1")
+# userID1 = db.create_user("Bitch")
 # print("UserID:",userID1)
-# db.submit_score(random.randint(5, 150), 1, userID1)
-# db.get_highscore(userID1)
+# db.submit_score(2, random.randint(0, 2), userID1)
+# print("HIGHSCORE:",db.get_highscore(1,userID1))
 
-# userID2 = db.create_user("JohnnyMyBoy")
+# userID2 = db.create_user("Desater")
 # print("UserID:",userID2)
-# db.submit_score(random.randint(5, 150), 2, userID2)
-# db.get_highscore(userID2)
+# db.submit_score(17, random.randint(0, 2), userID2)
+# print("HIGHSCORE:",db.get_highscore(2,userID2))
 
 # userID3 = db.create_user("Skillplayer99")
 # print("UserID:",userID3)
-# db.submit_score(random.randint(5, 150), 1, userID3)
+# db.submit_score(random.randint(5, 150), print(random.randint(0, 2)), userID3)
 # print(db.get_highscore(userID3))
 
-# print("\nAll Data:")
-# db.get_all_data()
+print("\nAll Data:")
+db.get_all_data()
 
-# print("\nLeaderboard:")
-# leaderboard = db.get_leaderboard()
-# print(leaderboard)
+print("\nLeaderboard:")
+leaderboard = db.get_leaderboard()
+print(leaderboard)
+
+print("\nName:")
+specific = db.get_user(leaderboard[1][0][0])[1]
+print(specific)
